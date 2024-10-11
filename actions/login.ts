bd048eb59eac77f0cd45daeb52541ba46d642f3d
pedrohmelo <phmc_1995@hotmail.com>
2024-10-11 11:51:58 -0300

    feat: added signout funcionality

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import { AuthError } from "next-auth";
 
+import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   console.log(values);
@@ -13,5 +16,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid Fields" };
   }
 
-  return { success: "Email Sent!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid Credentials!" };
+        default:
+          return { error: "Something went wrong" };
+      }
+    }
+
+    throw error;
+  }
 };
